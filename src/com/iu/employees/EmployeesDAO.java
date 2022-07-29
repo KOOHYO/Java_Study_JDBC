cc1d41a1108416b6ceab0895b3e3fa45924eba51
KOOHYO <zpfhzpfh112@naver.com>
2022-07-29 17:44:07 +0900

    ㅎㅎ

--- src/com/iu/employees/EmployeesDAO.java
+++ src/com/iu/employees/EmployeesDAO.java
@@ -9,6 +9,41 @@ import com.iu.util.DBConnector;
 
 public class EmployeesDAO {
 
+	public void getJoinTest(EmployeesDTO employeesDTO) throws Exception{
+		
+		//1. DB 연결
+		Connection con = DBConnector.getConnection();
+		//DB에는 \r\n이 없으니 빼고 띄어쓰기( )한번 해야한다
+		//2. SQL문 생성
+		String sql = "SELECT E.LAST_NAME, E.SALARY, D.DEPARTMENT_NAME "
+				+ "FROM EMPLOYEES E "
+				+ "    INNER JOIN "
+				+ "    DEPARTMENTS D "
+				+ "    ON E.DEPARTMENT_ID = D.DEPARTMENT_ID "
+				+ "WHERE E.EMPLOYEE_ID = 178";//마지막 값은 ?로 해야한다
+		
+		//3. 미리 전송
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		//4. ? 세팅
+		st.setInt(1, employeesDTO.getEmployee_id());
+		
+		//5. 전송 후 결과 처리
+		ResultSet rs = st.executeQuery();
+		
+		if(rs.next()) {
+			employeesDTO = new EmployeesDTO();
+			employeesDTO.setLast_name(rs.getString("LAST_NAME"));
+			employeesDTO.setSalary(rs.getDouble("SALARY"));
+			
+			
+		}
+		
+		//6. 연결 해제
+		DBConnector.disConnect(rs, st, con);//클래스 메서드 
+		
+	}
+	
 	public void getSalaryInfo() throws Exception {
 		Connection con = DBConnector.getConnection();
 		String sql = "SELECT SUM(SALARY), AVG(SALARY), MAX(SALARY) FROM EMPLOYEES";
